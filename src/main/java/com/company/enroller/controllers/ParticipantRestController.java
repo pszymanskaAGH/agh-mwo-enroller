f161fe2b6c76054b1a22af7765570696458a2168
Your Name <you@example.com>
2025-03-16 15:49:41 +0100

    test

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -1,28 +1,86 @@
 package com.company.enroller.controllers;
 
-import java.util.Collection;
-
+import com.company.enroller.model.Participant;
+import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.company.enroller.model.Participant;
-import com.company.enroller.persistence.ParticipantService;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/participants")
 public class ParticipantRestController {
 
-	@Autowired
-	ParticipantService participantService;
+    @Autowired
+    ParticipantService participantService;
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public ResponseEntity<?> getParticipants() {
-		Collection<Participant> participants = participantService.getAll();
-		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
-	}
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> getParticipants() {
+        Collection<Participant> participants = participantService.getAll();
+        return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+    }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
+        Participant participant = participantService.findByLogin(login);
+        if (participant == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(participant, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+        if (participant.getLogin() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (participant.getPassword() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Participant p = participantService.findByLogin(participant.getLogin());
+        if (p != null) {
+            return new ResponseEntity<>("Unable to create. A participant with login " +
+                    participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
+        }
+
+        participantService.addUser(participant);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteUser(@PathVariable("id") String login) {
+        Participant participant = participantService.findByLogin(login);
+        if (participant == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        participantService.deleteUser(participant);
+        return new ResponseEntity<>(participant, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateUserPass(@RequestBody Participant participant) {
+        Participant copy = participantService.findByLogin(participant.getLogin());
+        if (copy == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        copy.setPassword(participant.getPassword());
+        participantService.updateUser(copy);
+        return new ResponseEntity<>(copy, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/sortDESC", method = RequestMethod.PUT)
+    public ResponseEntity<?> sortDesc() {
+        participantService.sortDesc();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/sortASC", method = RequestMethod.PUT)
+    public ResponseEntity<?> sortASC() {
+        participantService.sortAsc();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -1,25 +1,70 @@
 package com.company.enroller.persistence;
 
-import java.util.Collection;
-
+import com.company.enroller.model.Participant;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
-import com.company.enroller.model.Participant;
+import java.util.Collections;
+import java.util.List;
 
 @Component("participantService")
 public class ParticipantService {
 
-	DatabaseConnector connector;
+    DatabaseConnector connector;
 
-	public ParticipantService() {
-		connector = DatabaseConnector.getInstance();
-	}
+    public ParticipantService() {
+        connector = DatabaseConnector.getInstance();
+    }
 
-	public Collection<Participant> getAll() {
-		String hql = "FROM Participant";
-		Query query = connector.getSession().createQuery(hql);
-		return query.list();
-	}
+    public List getAll() {
+        String hql = "FROM Participant";
+        Query query = connector.getSession().createQuery(hql);
+        return query.list();
+    }
 
+    public Participant findByLogin(String login) {
+        String hql = "FROM Participant Where login = '" + login + "'";
+        Query query = connector.getSession().createQuery(hql);
+        Participant participant = (Participant) query.list().get(0);
+        return participant;
+    }
+
+    public void addUser(Participant participant) {
+        Transaction tx = connector.getSession().beginTransaction();
+        connector.getSession().save(participant);
+        tx.commit();
+    }
+
+    public void deleteUser(Participant participant) {
+        Transaction tx = connector.getSession().beginTransaction();
+        connector.getSession().delete(participant);
+        tx.commit();
+    }
+
+    public void updateUser(Participant participant) {
+        Transaction tx = connector.getSession().beginTransaction();
+        connector.getSession().update(participant);
+        tx.commit();
+    }
+
+    public void sortDesc() {
+        Transaction tx = connector.getSession().beginTransaction();
+        String hql = "FROM Participant";
+        Query query = connector.getSession().createQuery(hql);
+        List list = query.list();
+        list.sort(Collections.reverseOrder());
+        connector.getSession().update(list);
+        tx.commit();
+    }
+
+    public void sortAsc() {
+        Transaction tx = connector.getSession().beginTransaction();
+        String hql = "FROM Participant";
+        Query query = connector.getSession().createQuery(hql);
+        List list = query.list();
+        Collections.sort(list);
+        connector.getSession().update(list);
+        tx.commit();
+    }
 }
