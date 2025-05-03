4f4f1e8273cedca00d47c6fd31491a0a4af010b1
Paulina Szymanska <paulina.szymanska1997@interia.pl>
2025-05-03 06:11:17 +0200

    add last method

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -133,76 +133,14 @@ public class MeetingRestController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    //3 podpunktcik z dalsze kroki
     @RequestMapping(value = "/getParticipantsFromMeeting", method = RequestMethod.GET)
     public ResponseEntity<?> getParticipantsFromMeeting(@RequestBody Meeting meeting) {
-        Collection<Participant> participants = meeting.getParticipants();
+        Meeting m = meetingService.findById(meeting.getId());
+        Collection<Participant> participants = m.getParticipants();
         if (meeting.getId() == 0 || meeting.getDate() == null || meeting.getDescription() == null || meeting.getTitle() == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        Meeting m = meetingService.findById(meeting.getId());
-//        m.
-
-//        for (Participant participant : participants) {
-//            if (participant.getLogin() == null || participant.getLogin().isEmpty()) {
-//                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//            }
-//
-//            if (p == null) {
-//                return new ResponseEntity<>("Unable to add participant to meeting, because participant is null", HttpStatus.NOT_FOUND);
-//            }
-//            if (m == null) {
-//                return new ResponseEntity<>("Unable to add participant to meeting, because meeting is null", HttpStatus.NOT_FOUND);
-//            }
-//
-//            meetingService.removeParticipantFromMeeting(participant, meeting);
-//
-//        }
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(participants, HttpStatus.OK);
     }
 }
-//#POST http://localhost:8090/meetings/
-//        #Content-Type: application/json
-//#
-//        #
-//        #  {
-//        #    "id": 3,
-//        #    "title": "some title",
-//        #    "description": "some description",
-//        #    "date": "some date"
-//        #  }
-//
-//
-//        #DELETE http://localhost:8090/meetings/3
-//
-//        #GET http://localhost:8090/participants/
-//
-//        #PUT http://localhost:8090/participants/sortDESC
-//        #PUT http://localhost:8090/participants/keyUser
-//
-//        #PUT http://localhost:8090/meetings/2
-//        #Content-Type: application/json
-//#
-//        #"date"
-//
-//
-//PUT http://localhost:8090/meetings/removeParticipant
-//Content-Type: application/json
-//
-//{
-//    "id": 3,
-//        "title": "some title",
-//        "description": "some description",
-//        "date": "some date",
-//        "participants": [
-//    {
-//        "login": "user2",
-//            "password": "some password"
-//    }
-//  ]
-//}
-//
-//
-//
-//
